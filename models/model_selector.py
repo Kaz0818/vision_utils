a5ffc0bb97ac57f1a73e2e95fe812b5fff07cbad
Kaz0818 <120219262+Kaz0818@users.noreply.github.com>
2025-05-20 10:41:20 +0900

    Models/load model (#4)
    * feature: change flexible load_model function
    
    * delete: because make load_model function

--- models/model_selector.py
+++ models/model_selector.py
@@ -1,15 +1,33 @@
-from vision_utils.models.model_cnn import SimpleCNN
-from vision_utils.models.resnet_finetune import build_resnet18
+import torch.nn as nn
+import torchvision
 
-def get_model(name, num_classes=10):
-    """
-    name: str: configs/config.yamlに書いてある。nameを入れる。例: 'simple_cnn'
+def load_model(in_channels: int, model_name: str, num_classes: int, pretrained: bool = True, feature_extract: bool = False):
+    model_name = model_name.lower()
+    
+    if model_name == 'resnet18':
+        model = torchvision.models.resnet18(weights='DEFAULT' if pretrained else None)
+        # 入力チャンネルが3以外の場合のみ、最初のConvを上書き
+        if in_channels != 3:
+            model.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        # 最終層の置き換え
+        model.fc = nn.Linear(model.fc.in_features, num_classes)
+        # 転移学習モード（最終層だけ学習）
+        if feature_extract:
+            for param in model.parameters():
+                param.requires_grad = False
+            model.fc = nn.Linear(model.fc.in_features, num_classes)  # ←この書き換えでfcだけrequires_grad=Trueになる
 
-    num_classes: int: outputのクラス数を指定。
-    """
-    if name.lower() == 'simple_cnn':
-        return SimpleCNN(num_classes=num_classes)
-    elif name.lower() == 'resnet18':
-        return build_resnet18(num_classes=num_classes)
+    elif model_name == 'vgg16':
+        model = torchvision.models.vgg16(weights='DEFAULT' if pretrained else None)
+        if in_channels != 3:
+            model.features[0] = nn.Conv2d(in_channels, 64, kernel_size=3, padding=1)
+        model.classifier[6] = nn.Linear(model.classifier[6].in_features, num_classes)
+        if feature_extract:
+            for param in model.parameters():
+                param.requires_grad = False
+            model.classifier[6] = nn.Linear(model.classifier[6].in_features, num_classes)  # ここでclassifier
+
     else:
-        raise ValueError(f"Unknown model name: {model}")
+        raise ValueError(f"Unknown model name: {model_name}")
+
+    return model
